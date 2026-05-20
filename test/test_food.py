06452e7e17e2047d399672e0a49234ff278382e9
glennputra04 <glennputralaymando4@gmail.com>
2026-05-20 14:15:39 +0700

    Feat: NonPeak_Premium test

--- test/test_food.py
+++ test/test_food.py
@@ -1,103 +1,15 @@
 import pytest
-from src.fare import compute_bus_fare
+from src.food import calculate_delivery_fee
 
 
-def test_children_under_2_free():
-    assert compute_bus_fare(
-        age=1,
-        day_type="weekday",
-        hour=10,
-        trip_duration=20,
-        is_public_holiday=False
+
+#region TC019 - TC024
+def test_tc019():
+    assert calculate_delivery_fee(
+        order_amount=100,
+        delivery_time="NON_PEAKHOURS",
+        membership_status="PREMIUM",
+        delivery_distance=19
     ) == 0
 
-
-def test_child_half_fare():
-    assert compute_bus_fare(
-        age=10,
-        day_type="weekday",
-        hour=10,
-        trip_duration=20,
-        is_public_holiday=False
-    ) == 1.5
-
-
-def test_senior_half_fare():
-    assert compute_bus_fare(
-        age=70,
-        day_type="weekday",
-        hour=10,
-        trip_duration=20,
-        is_public_holiday=False
-    ) == 1.5
-
-
-def test_adult_regular_fare():
-    assert compute_bus_fare(
-        age=30,
-        day_type="weekday",
-        hour=10,
-        trip_duration=20,
-        is_public_holiday=False
-    ) == 3
-
-
-def test_peak_hour_surcharge():
-    assert compute_bus_fare(
-        age=30,
-        day_type="weekday",
-        hour=8,
-        trip_duration=20,
-        is_public_holiday=False
-    ) == 4.5
-
-
-def test_weekend_flat_fare():
-    assert compute_bus_fare(
-        age=30,
-        day_type="weekend",
-        hour=10,
-        trip_duration=20,
-        is_public_holiday=False
-    ) == 2
-
-
-def test_short_trip_free():
-    assert compute_bus_fare(
-        age=30,
-        day_type="weekday",
-        hour=11,
-        trip_duration=3,
-        is_public_holiday=False
-    ) == 0
-
-
-def test_public_holiday_override():
-    assert compute_bus_fare(
-        age=30,
-        day_type="weekday",
-        hour=8,
-        trip_duration=20,
-        is_public_holiday=True
-    ) == 5
-
-
-def test_public_holiday_child():
-    assert compute_bus_fare(
-        age=10,
-        day_type="weekday",
-        hour=8,
-        trip_duration=20,
-        is_public_holiday=True
-    ) == 3.5
-
-
-def test_invalid_age():
-    with pytest.raises(ValueError):
-        compute_bus_fare(
-            age=-1,
-            day_type="weekday",
-            hour=10,
-            trip_duration=20,
-            is_public_holiday=False
-        )
+#endregion

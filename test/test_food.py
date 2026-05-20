dc0183a3f666609e3f73cb0a5786e9eab165496b
glennputra04 <glennputralaymando4@gmail.com>
2026-05-20 14:16:41 +0700

    Feat: NonPremium but high order

--- test/test_food.py
+++ test/test_food.py
@@ -11,7 +11,7 @@ def test_tc019():
         membership_status="PREMIUM",
         delivery_distance=19
     ) == 0
-    
+
 def test_tc020():
     assert calculate_delivery_fee(
         order_amount=67,
@@ -19,4 +19,12 @@ def test_tc020():
         membership_status="PREMIUM",
         delivery_distance=25
     ) == "Invalid"
+
+def test_tc021():
+    assert calculate_delivery_fee(
+        order_amount=6767,
+        delivery_time="PEAK_HOURS",
+        membership_status="NON_PREMIUM",
+        delivery_distance=14
+    ) == 0
 #endregion
